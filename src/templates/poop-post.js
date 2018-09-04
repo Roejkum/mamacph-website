e999670c6cd2d441a615089b937cd888e0d48e83
Mads Røjkum Nielsen <roejkum@gmail.com>
2018-09-04 22:15:56 +0200

    asd

--- src/templates/poop-post.js
+++ src/templates/poop-post.js
@@ -5,7 +5,7 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import Content, { HTMLContent } from '../components/Content'
 
-export const BlogPostTemplate = ({
+export const PoopPostTemplate = ({
   content,
   contentComponent,
   description,
@@ -45,7 +45,7 @@ export const BlogPostTemplate = ({
   )
 }
 
-BlogPostTemplate.propTypes = {
+PoopPostTemplate.propTypes = {
   content: PropTypes.string.isRequired,
   contentComponent: PropTypes.func,
   description: PropTypes.string,
@@ -53,31 +53,31 @@ BlogPostTemplate.propTypes = {
   helmet: PropTypes.instanceOf(Helmet),
 }
 
-const BlogPost = ({ data }) => {
+const PoopPost = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
-    <BlogPostTemplate
+    <PoopPostTemplate
       content={post.html}
       contentComponent={HTMLContent}
       description={post.frontmatter.description}
-      helmet={<Helmet title={`${post.frontmatter.title} | Blog`} />}
+      helmet={<Helmet title={`${post.frontmatter.title} | Poop`} />}
       tags={post.frontmatter.tags}
       title={post.frontmatter.title}
     />
   )
 }
 
-BlogPost.propTypes = {
+PoopPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
 }
 
-export default BlogPost
+export default PoopPost
 
 export const pageQuery = graphql`
-  query BlogPostByID($id: String!) {
+  query PoopPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
