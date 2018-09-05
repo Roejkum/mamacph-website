8dcfb4b15270b2839d3812ee3278b6f228d60021
Mads Røjkum Nielsen <roejkum@gmail.com>
2018-09-05 20:23:06 +0200

    Pis man. Glemte lige at inkluderer det i garphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,6 +66,7 @@ export const pageQuery = graphql`
           }
           frontmatter {
             title
+            path
             templateKey
             manchet
             date(formatString: "MMMM DD, YYYY")
