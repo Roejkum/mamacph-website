97c9dd038bfdbcfee3c1925490e420422db0c105
Mads Røjkum Nielsen <roejkum@gmail.com>
2018-09-05 13:35:04 +0200

    sdf

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,7 +33,7 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     posts.forEach(edge => {
       const id = edge.node.id
       createPage({
-        path: edge.node.fields.slug,
+        path: edge.node.frontmatter.title,
         tags: edge.node.frontmatter.tags,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
