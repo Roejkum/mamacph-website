8f2ea137fa4a8a255a5177c10c77ccc2633b2e3b
Mads Røjkum Nielsen <roejkum@gmail.com>
2018-09-18 20:59:35 +0200

    asd

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'MAMA - Digitale interaktive løsninger',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
