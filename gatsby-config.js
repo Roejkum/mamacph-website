d14c1f33dfd16647912e2d0e0da5b20470707dd2
Mads Røjkum <roejkum@gmail.com>
2018-12-14 23:02:43 +0100

    manifest and meta descr

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,20 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MAMA`,
+        short_name: `MAMA`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/img/mamalogo.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -8,6 +8,7 @@ const Layout = ({ children }) => (
   <>
     <Helmet> 
       <title>MAMA | Digitale interaktive løsninger</title>
+      <meta name="description" content="Web design - web developmemt " />
       <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
     </Helmet>
     <Navbar />
