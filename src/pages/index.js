e4968b216606872e98edc8dd95644e6255b891a4
Mads Røjkum <roejkum@gmail.com>
2018-12-17 10:14:53 +0100

    add webp support

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,6 +98,7 @@ export const pageQuery = graphql`
                   srcSet
                   aspectRatio
                   base64
+                  srcSetWebp
                 }
               }
             }
